df8394bbef1ead24b694f30a64a258ba1a4f9e4b
Jack Wright <jack.wright@disqo.com>
2024-02-02 13:51:29 -0800

    to plist

--- src/nu_plist.rs
+++ src/nu_plist.rs
@@ -3,40 +3,75 @@ use std::time::SystemTime;
 use chrono::{DateTime, FixedOffset, Offset, Utc};
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
 use nu_protocol::{Category, PluginExample, PluginSignature, Record, Span, Type, Value as NuValue};
-use plist::{Date as PlistDate, Dictionary, Value as PlistValue};
+use plist::{Date as PlistDate, Dictionary, Integer, Value as PlistValue};
 
 pub struct NuPlist;
 
 impl Plugin for NuPlist {
     fn signature(&self) -> Vec<PluginSignature> {
-        vec![PluginSignature::build("from plist")
-            .input_output_types(vec![(Type::String, Type::Any)])
-            .usage("Parse text as an Apple plist document")
-            .plugin_examples(vec![PluginExample {
-                example: "cat file.plist | from plist".to_string(),
-                description: "Convert a plist file to a table".to_string(),
-                result: None,
-            }])
-            .category(Category::Formats)]
+        vec![
+            PluginSignature::build("from plist")
+                .input_output_types(vec![(Type::String, Type::Any)])
+                .usage("Parse text as an Apple plist document")
+                .plugin_examples(vec![PluginExample {
+                    example: "cat file.plist | from plist".to_string(),
+                    description: "Convert a plist file to a table".to_string(),
+                    result: None,
+                }])
+                .category(Category::Formats),
+            PluginSignature::build("to plist")
+                .usage("Convert Nu values into plist")
+                .switch("binary", "Output plist in binary format", Some('b'))
+                .plugin_examples(vec![PluginExample {
+                    example: "{ a: 3 } | to plist".to_string(),
+                    description: "Convert a table into a plist file".to_string(),
+                    result: None,
+                }])
+                .category(Category::Formats),
+        ]
     }
 
     fn run(
         &mut self,
-        _name: &str,
+        name: &str,
         call: &EvaluatedCall,
         input: &NuValue,
     ) -> Result<NuValue, LabeledError> {
-        match input {
-            NuValue::String { val, .. } => {
-                let plist = plist::from_bytes(val.as_bytes())
-                    .map_err(|e| build_label_error(format!("{}", e), &call.head))?;
-                let converted = convert_plist_value(&plist)?;
-                Ok(converted)
+        if name == "from plist" {
+            match input {
+                NuValue::String { val, .. } => {
+                    let plist = plist::from_bytes(val.as_bytes())
+                        .map_err(|e| build_label_error(format!("{}", e), &input.span()))?;
+                    let converted = convert_plist_value(&plist)?;
+                    Ok(converted)
+                }
+                NuValue::Binary { val, .. } => {
+                    let plist = plist::from_bytes(val)
+                        .map_err(|e| build_label_error(format!("{}", e), &input.span()))?;
+                    let converted = convert_plist_value(&plist)?;
+                    Ok(converted)
+                }
+                _ => Err(build_label_error(
+                    format!("Invalid input, must be string not: {:?}", input),
+                    &call.head,
+                )),
             }
-            _ => Err(build_label_error(
-                format!("Invalid input, must be string not: {:?}", input),
-                &call.head,
-            )),
+        } else {
+            let plist_val = convert_nu_value(input)?;
+            let mut out = Vec::new();
+            if call.has_flag("binary") {
+                plist::to_writer_binary(&mut out, &plist_val)
+                    .map_err(|e| build_label_error(format!("{}", e), &input.span()))?;
+                Ok(NuValue::binary(out, input.span()))
+            } else {
+                plist::to_writer_xml(&mut out, &plist_val)
+                    .map_err(|e| build_label_error(format!("{}", e), &input.span()))?;
+                Ok(NuValue::string(
+                    String::from_utf8(out)
+                        .map_err(|e| build_label_error(format!("{}", e), &input.span()))?,
+                    input.span(),
+                ))
+            }
         }
     }
 }
@@ -90,6 +125,44 @@ pub fn convert_date(plist_date: &PlistDate) -> DateTime<FixedOffset> {
     let utc_date: DateTime<Utc> = plist_sys_time.into();
     let utc_offset = utc_date.offset().fix();
     utc_date.with_timezone(&utc_offset)
+}
+
+fn convert_nu_value(nu_val: &NuValue) -> Result<PlistValue, LabeledError> {
+    let span = Span::test_data();
+    match nu_val {
+        NuValue::String { val, .. } => Ok(PlistValue::String(val.to_owned())),
+        NuValue::Bool { val, .. } => Ok(PlistValue::Boolean(*val)),
+        NuValue::Float { val, .. } => Ok(PlistValue::Real(*val)),
+        NuValue::Int { val, .. } => Ok(PlistValue::Integer(Into::<Integer>::into(*val))),
+        NuValue::Binary { val, .. } => Ok(PlistValue::Data(val.to_owned())),
+        NuValue::Record { val, .. } => convert_nu_dict(val),
+        NuValue::List { vals, .. } => Ok(PlistValue::Array(
+            vals.iter()
+                .map(convert_nu_value)
+                .collect::<Result<_, _>>()?,
+        )),
+        NuValue::Date { val, .. } => Ok(PlistValue::Date(SystemTime::from(val.to_owned()).into())),
+        NuValue::LazyRecord { val, .. } => {
+            let record = val.collect()?;
+            let record = record
+                .as_record()
+                .map_err(|e| build_label_error(format!("{}", e), &span))?;
+            convert_nu_dict(record)
+        }
+        _ => Err(build_label_error(
+            format!("{:?} is not convertible", nu_val),
+            &span,
+        )),
+    }
+}
+
+fn convert_nu_dict(record: &Record) -> Result<PlistValue, LabeledError> {
+    Ok(PlistValue::Dictionary(
+        record
+            .iter()
+            .map(|(k, v)| convert_nu_value(v).map(|v| (k.to_owned(), v)))
+            .collect::<Result<_, _>>()?,
+    ))
 }
 
 #[cfg(test)]
